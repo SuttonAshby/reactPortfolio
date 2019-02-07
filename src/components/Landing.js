ef5ac8fab86323d1b7cd43e9b856d2aabcc7f041
Sutton Ashby <korim_fidelis@yahoo.com>
2019-02-06 22:47:30 -0500

    conditional for triangle

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import background from '../images/texture-bg.jpg';
 import UpTri from './UpTri.js'
 
@@ -13,13 +14,18 @@ const styles = {
         height: "100vh",
         width: "100vw",
         maxWidth: "100%",
-        backgroundImage: `url(${background})`
+        backgroundImage: `url(${background})`,
+
     },
     upperRow: {
         height: "50vh",
         width: "100vw",
         maxWidth: "100%",    
-        background: "rgba(255,255,255,.5)"            
+        background: "rgba(255,255,255,.5)",
+        // justifyContent: "center",
+        // alignItems: "center",
+        // flexDirection: "row",
+        // flexGrow: 1
     }
   };
 
@@ -28,9 +34,14 @@ function Landing(props) {
 
     return (
         <div style={styles.landing}>
-            <div style={styles.upperRow}>
+            <Grid container style={styles.upperRow}
+                justifyContent="center"
+                alignItems= "center"
+            >
+                <UpTri up="true" />
                 <UpTri />
-            </div>
+                <UpTri up="true" />
+            </Grid>
         </div>
     )
 }
--- src/components/UpTri.js
+++ src/components/UpTri.js
@@ -2,24 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
+
 
 const styles = {
     UpTri:{
         clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
         background: "rgba(255,255,255,1)",
         width: '33vw',
-        height: 'calc(33vw/1.5)'    }
+        height: 'calc(33vw/1.5)'
+    },
+    DownTri:{
+        clipPath: "polygon(50% 100%, 0 0, 100% 0)",
+        background: "rgba(255,255,255,1)",
+        width: '33vw',
+        height: 'calc(33vw/1.5)'        
+    }
 }   
 
-function UpTri (props) {
-
-return(
-    <div style={styles.UpTri}>
-
-    </div>
-)
+function Tri (props) {
+    if(props.up){
+        return <div style={styles.UpTri} />
+    } 
+    return <div style={styles.DownTri} />
 
 }
 
-export default UpTri;
+export default Tri;
