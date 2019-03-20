3fef94a8a6bbb00febb5aa309939a6ac6263d317
Sutton Ashby <korim_fidelis@yahoo.com>
2019-03-19 23:51:18 -0400

    converting portfolioPiece to component

--- src/components/PortfolioPiece.js
+++ src/components/PortfolioPiece.js
@@ -5,52 +5,56 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import { Card, CardMedia } from '@material-ui/core';
 
-function PortfolioPiece (props) {
-
-    const styles = {
-        imagePositioning: {
-            position: "absolute",
-            top: 0,
-            left: 0,
-            backgroundSize: "cover"
-        },
-        imageContainer: {
-            width: 300,
-            height: 300,
-            position: "relative"
-        },
-        siteImage: {
-            backgroundImage:  `url(${props.siteImage})`,
-            width: 300,
-            height: 300,
-            clipPath: "polygon(0 0, 100% 100%, 100% 0)"
-        },
-        codeImage: {
-            backgroundImage:  `url(${props.codeImage})`,
-            width: 300,
-            height: 300,
-            clipPath: "polygon(0 0, 0% 100%, 100% 100%)"
-        },
-        siteImageSelected: {
-            
-        },
-        codeImageSelected: {
-
+class PortfolioPiece extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imagePositioning: {
+                position: "absolute",
+                top: 0,
+                left: 0,
+                backgroundSize: "cover"
+            },
+            imageContainer: {
+                width: 300,
+                height: 300,
+                position: "relative"
+            },
+            siteImage: {
+                backgroundImage:  `url(${props.siteImage})`,
+                width: 300,
+                height: 300,
+                clipPath: "polygon(0 0, 100% 100%, 100% 0)"
+            },
+            codeImage: {
+                backgroundImage:  `url(${props.codeImage})`,
+                width: 300,
+                height: 300,
+                clipPath: "polygon(0 0, 0% 100%, 100% 100%)"
+            },
+            siteImageSelected: {
+    
+            },
+            codeImageSelected: {
+    
+            }   
         }
-
     }
-    return (
-        <Grid item>
+ 
+    render() {
+        return (
+            <Grid item>
             <Card>
                 <Typography>{props.title}</Typography>
                 <Typography>{props.blurb}</Typography>
-                <div style={styles.imageContainer}>
-                    <div style={{...styles.imagePositioning, ...styles.codeImage}} />
-                    <div style={{...styles.imagePositioning, ...styles.siteImage}} />   
+                <div style={this.state.imageContainer}>
+                    <div style={{...this.state.imagePositioning, ...this.state.codeImage}} />
+                    <div style={{...this.state.imagePositioning, ...this.state.siteImage}} />   
                 </div>
             </Card>
         </Grid>
-    )
+        )
+    }
 }
 
 export default PortfolioPiece;
